d4361971ec303ab3a396c48061f0c0365836a447
Plamen <pazenze@gmail.com>
2025-02-11 17:10:16 +0200

    Created .env.default and .gitignore files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  eslintPluginPrettier,
-  {
-    rules: {
-      'capitalized-comments': ['error', 'always'],
-    },
-  },
+  ...tseslint.configs.recommended,
 ];
